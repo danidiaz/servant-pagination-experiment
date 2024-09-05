26b17696f23600bcf92dfee83d302a3d721751da
Daniel Díaz <diaz_carrete@yahoo.com>
2024-09-05 20:34:51 +0200

    building the server

--- app/Main.hs
+++ app/Main.hs
@@ -5,19 +5,25 @@ import Servant.Client
 import Servant.API
 import Servant.API.NamedRoutes
 import Servant.Server
+import Servant.Links
+import Data.Proxy
+import GHC.Generics (Generic)
 
 type PaginatedApi = NamedRoutes Foo
 
 data Foo mode = Foo {
     firstContent :: mode :- "contents" :> Get '[JSON] (Headers '[Header "Link" String] [Int]),
-    extraContent :: mode :- "contents-extra" :> QueryParam "page" Int :>  Headers '[Header "Link" String] [Int]
-} 
+    extraContent :: mode :- "contents-extra" :> QueryParam "page" Int :>  Get '[JSON] (Headers '[Header "Link" String] [Int])
+} deriving stock (Generic)
 
 fooServer :: Server PaginatedApi
 fooServer = Foo {
         firstContent = undefined, -- :: "contents" :> Get '[JSON] (Headers '[Header "Link" String] [Int]),
         extraContent = \i -> undefined
     }
+
+fooRoutes :: MkLink PaginatedApi URI 
+fooRoutes = allLinks' linkURI (Proxy @PaginatedApi)
 
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
