3247d0aa71f5be0af31333896ed9be82d7f42b72
Daniel Díaz <diaz_carrete@yahoo.com>
2024-09-05 20:26:52 +0200

    ...

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,23 @@
 module Main where
 
+import Network.HTTP.Client.Internal qualified as Http
+import Servant.Client
+import Servant.API
+import Servant.API.NamedRoutes
+import Servant.Server
+
+type PaginatedApi = NamedRoutes Foo
+
+data Foo mode = Foo {
+    firstContent :: mode :- "contents" :> Get '[JSON] (Headers '[Header "Link" String] [Int]),
+    extraContent :: mode :- "contents-extra" :> QueryParam "page" Int :>  Headers '[Header "Link" String] [Int]
+} 
+
+fooServer :: Server PaginatedApi
+fooServer = Foo {
+        firstContent = undefined, -- :: "contents" :> Get '[JSON] (Headers '[Header "Link" String] [Int]),
+        extraContent = \i -> undefined
+    }
+
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
