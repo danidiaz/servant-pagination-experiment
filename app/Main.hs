fe0c7ca3699ee74f14044e6ea0bd3f2785b38f4c
Daniel Díaz <diaz_carrete@yahoo.com>
2024-09-05 21:04:28 +0200

    server working

--- app/Main.hs
+++ app/Main.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE OverloadedRecordDot #-}
 module Main where
 
 import Network.HTTP.Client.Internal qualified as Http
@@ -7,23 +8,41 @@ import Servant.API.NamedRoutes
 import Servant.Server
 import Servant.Links
 import Data.Proxy
+import Data.Text
 import GHC.Generics (Generic)
+import Network.URI (uriToString)
+import Data.Function ((&))
+import Network.Wai.Handler.Warp (run)
+import Servant (throwError)
 
 type PaginatedApi = NamedRoutes Foo
 
 data Foo mode = Foo {
     firstContent :: mode :- "contents" :> Get '[JSON] (Headers '[Header "Link" String] [Int]),
-    extraContent :: mode :- "contents-extra" :> QueryParam "page" Int :>  Get '[JSON] (Headers '[Header "Link" String] [Int])
+    extraContent :: mode :- "contents-extra" :> Capture "page" Int :>  Get '[JSON] (Headers '[Header "Link" String] [Int])
 } deriving stock (Generic)
 
 fooServer :: Server PaginatedApi
 fooServer = Foo {
-        firstContent = undefined, -- :: "contents" :> Get '[JSON] (Headers '[Header "Link" String] [Int]),
-        extraContent = \i -> undefined
+        firstContent = do
+                pure $ addHeader (nextLink 0) [1,2,3]
+            , -- :: "contents" :> Get '[JSON] (Headers '[Header "Link" String] [Int]),
+
+        extraContent = \case
+            0 -> do
+                pure $ addHeader (nextLink 1) [3,4,5]
+            1 -> do
+                pure $ noHeader [3,4,5]
+            _ -> throwError err404
     }
+    where 
+    nextLink i = uriToString id (fooRoutes.extraContent i) ""
 
 fooRoutes :: MkLink PaginatedApi URI 
 fooRoutes = allLinks' linkURI (Proxy @PaginatedApi)
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    putStrLn "Starting server!!!"
+    let application :: Application = fooServer & serve (Proxy @PaginatedApi)
+    application & run 8000
